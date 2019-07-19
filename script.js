681118f51d0deb1a4453f577d23b301e32218664
Thiago Mendes <thiagomendessan293@gmail.com>
2019-07-19 17:37:59 -0300

    Adicionar Atividades

---
+++ script.js
@@ -0,0 +1,90 @@
+// let idade = Number(prompt("Digite sua idade"));
+// let aposentado = prompt(" Você é aposentado?")
+
+// let estudante = prompt("Voce é estudante") 
+
+// if(idade >= 60 || aposentado == "sim"){
+//     alert("Entrada Grátis");
+// }
+
+// else if(idade >= 0 && idade <= 12){
+//     alert("Sua entrada é R$10,00 ");
+// }
+// else if(idade >= 13 && idade <= 17){
+//     alert("Sua entrada é R$15,00 ");
+// }
+// else if(idade >= 18 && idade <= 59){
+//     if(estudante == "sim"){
+//         alert("Entrada R$17,50 meia entrada");
+//     } else {
+//         alert("Entrada R$35,00");
+//     }
+// }
+// else {
+//     alert("Tente novamente");
+// }
+
+
+
+
+// let semana = Number(prompt(""));
+
+// if(semana == 1){
+//     alert("É DOMINGO");
+// } else if (semana == 2){
+//     alert("É SEGUNDA");
+// } else if (semana == 3){
+//     alert("É TERÇA");
+// } else if (semana == 4){
+//     alert("É QUARTA");
+// } else if (semana == 5){
+//     alert("É QUINTA");
+// } else if (semana == 6){
+//     alert("É SEXTA");
+// } else if (semana == 7){
+//     alert("É SÁBADO");
+// }
+
+// else {
+//     alert("número inválido: digite um número de 1 a 7")
+// }
+
+
+
+
+// let idade = Number(prompt("Digite sua idade")); 
+
+
+// if(idade >= 25 && idade <= 60){
+//     let salario = Number(prompt("Digite seu salário"));
+//         if(salario >= 2000){
+//         let emprestimo = Number(prompt("Digite o emprestimo desejado"));
+//             if(emprestimo <10*salario){
+//                 alert("Emprestimo aprovado")
+//             }else{
+//                 alert("Emprestimo recusado, valor muito alto")
+//             }
+
+//         }else{
+//             alert("EMprestimo recusado, precisa ser no minimo 2000")
+//         }
+
+// }else{
+//     alert("Seu emprestimo foi negado por causa da idade")
+// }
+
+
+
+
+
+// let numero = Number(prompt("Digite seu numero de celular"));
+
+// if(numero[0] == "9" && numero.length == 9){
+//     alert("Numero aprovado")
+
+// }else{
+//     alert("Numero Reprovado")
+// }
+
+
+
